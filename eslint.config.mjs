f8c5e346465c05a8bf9e357879a3595e320aa119
atharvkulkarni2002 <atharvkulkarni378@gmail.com>
2024-07-16 15:59:40 +0530

    used pf-card and configured eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { fixupConfigRules } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...fixupConfigRules(compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+))];
