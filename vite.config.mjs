59667a2ebe559b73e3d81cb02011ed6dfb1027d0
Deepesh Padmanabhan Nair <hybridx18@gmail.com>
2024-07-23 18:52:09 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  base: `/${process.env.GITHUB_REPOSITORY?.split('/')[1]}/`,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
