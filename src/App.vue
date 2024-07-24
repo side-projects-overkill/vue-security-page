2de09f3c6376c1cec000b1d3814e10d3309ec6fe
hybridx <hybridx18@gmail.com>
2024-07-24 10:09:56 +0530

    fix(fixed-repo) - Added file changes

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@
 
 <script setup>
   import SecurityLandingHeader from './components/header/SecurityLandingHeader.vue'
-  import SecurityLandingFooter from './components/footer/SecurityLandingFooter.vue'
   import ProdsecBlogVue from './components/blog/ProdsecBlog.vue'
   import CveTable from './components/blog/CveTable.vue'
-  import SecurityHelpSection from './components/blog/SecurityHelpSection.vue'
   import ComplianceAndRiskSection from './components/blog/ComplianceAndRiskSection.vue'
+  import SecurityHelpSection from './components/blog/SecurityHelpSection.vue'
+  import SecurityLandingFooter from './components/footer/SecurityLandingFooter.vue'
 </script>
 
 <style scoped></style>
--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import dotenv from 'dotenv'
+
 // https://vitejs.dev/config/
+dotenv.config()
+
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   base: `/${process.env.GITHUB_REPOSITORY?.split('/')[1]}/`,
