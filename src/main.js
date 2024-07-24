0f61c2058a666798ff4bc2b033c63b2488dc9ebd
hybridx <hybridx18@gmail.com>
2024-07-24 16:10:44 +0530

    fix(dev-build) - Checking dev build for prod[exprimental]

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@rhds/elements/rh-card/rh-card.js'
-import '@rhds/elements/rh-cta/rh-cta.js'
-import '@rhds/elements/rh-accordion/rh-accordion.js'
 import '@patternfly/elements/pf-modal/pf-modal.js'
 import '@patternfly/elements/pf-card/pf-card.js'
 import '@patternfly/elements/pf-table/pf-table.js'
@@ -12,6 +9,9 @@ import '@patternfly/elements/pf-select/pf-select.js'
 import '@patternfly/elements/pf-select/pf-option-group.js'
 import '@patternfly/elements/pf-button/pf-button.js'
 import '@patternfly/elements/pf-icon/pf-icon.js'
+import '@rhds/elements/rh-card/rh-card.js'
+import '@rhds/elements/rh-cta/rh-cta.js'
+import '@rhds/elements/rh-accordion/rh-accordion.js'
 
 const app = createApp(App)
 
